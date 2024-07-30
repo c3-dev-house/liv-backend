5c910919e9d66ce5ac0d8fe9674c025b4b21ac17
louisdevc3 <117977837+louisdevc3@users.noreply.github.com>
2024-07-30 17:22:34 +0200

    louisdev | add preflight cors thingie

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 //app.use(cors());
+app.options('*', cors(corsOptions));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://127.0.0.1:5173");
   res.header("Access-Control-Allow-Credentials", "true");
